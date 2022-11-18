02e0d687c767b63f33feb92329c62be1b26a325d
Audureau Victor <victor.audureau@etudiant.univ-lr.fr>
2022-11-18 17:54:40 +0100

    Update CarteReseau.java

--- Projet/src/CarteReseau.java
+++ Projet/src/CarteReseau.java
@@ -68,30 +68,5 @@ public class CarteReseau {
         this.passerelle = passerelle;
     }
     
-    // Méthode permettant de générer une adresse MAC d'octet aléatoire
-public String genererMAC() {
-        
-    // Nombre de caractère dans une @MAC
-    final int NB_CARACTERE_MAC = 12;
-    String adresseMAC = "";
-    // Tableau des caractères possibles dans une @MAC
-    String caracteres[] = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F"};
-    // Compteur servant de repère pour placer un séparateur entre les caractères
-    int cpt = 0;
-
-    for (int i = 0; i < NB_CARACTERE_MAC; i++) {            
-        // On place un séparateur tout les 2 caractères
-        if (cpt == 2) {
-            adresseMAC += ":";
-            cpt = 0;
-        }
-        // Récupèration d'un caractère du tableau aléatoirement
-        String caractereAlea = caracteres[(int) (Math.random() * caracteres.length)];
-        // Concaténation du caractère aléatoire à la chaîne de caractère "adresseMAC"
-        adresseMAC += caractereAlea;
-        cpt++;
-    }
-    return adresseMAC;
-}
-    //
+   
 }
