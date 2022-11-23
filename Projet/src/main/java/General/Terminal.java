090391953b739cd3ca513b143c19998a72f73943
Victor Audureau <victor.audureau@gmail.com>
2022-11-23 11:05:43 +0100

    rangement fini

--- Projet/src/main/java/General/Terminal.java
+++ Projet/src/main/java/General/Terminal.java
@@ -2,13 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
+package General;
 
 /**
  *
  * @author victo
  */
-public class app {
-    public static void main(String[] args) {
-        System.out.println("pipi");
-    }
+public class Terminal {
+    
 }
