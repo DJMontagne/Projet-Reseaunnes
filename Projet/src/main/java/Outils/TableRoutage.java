6731aae710b050038db0e66b3ad36de542966d3e
Girard Arthur <arthur.girard@etudiant.univ-lr.fr>
2022-12-14 10:20:46 +0100

    Update Projet/src/main/java/Outils/TableRoutage.java

--- Projet/src/main/java/Outils/TableRoutage.java
+++ Projet/src/main/java/Outils/TableRoutage.java
@@ -7,7 +7,7 @@ import java.util.Map;
  * @author victo
  */
 
-public class TableRoutage {
+public class TableRoutage extends Table {
 
     // Nombre de caractères d'une passerelle
     private final int LONG_MAX_PASSERELLE = 15;
@@ -23,105 +23,16 @@ public class TableRoutage {
     /**
     * @param machine
     */
-    public TableRoutage(Machine machine) {
-        
-        this.tableRoutage = new HashMap<>();
-        initialisationTableRoutage(machine);
+    public TableRoutage() {
+        super.longMaxChampGauche = this.LONG_MAX_PASSERELLE;
+        super.longMaxChampMilieu = this.LONG_MAX_ADRESSE_IP;
+        super.longMaxChampDroite = this.LONG_NOM_INTERFACE;
+        super.affichageEnTete = this.contenuEnTeteAffichage;
+        super.table = new HashMap<Machine, String[]>();
+        super.longTotale = this.LONG_MAX_PASSERELLE + this.LONG_MAX_ADRESSE_IP + this.LONG_NOM_INTERFACE;
+       
     }
 
-    // Getter
-    public HashMap<Machine, String[]> getTableRoutage() {
-        
-        return this.tableRoutage;
-    }
-
-    /* 
-    Attendre d'avoir effectuer la classe Réseau car la table ARP d'une machine se remplie 
-    si cette dernière est connectée sur un réseau
-    */
-    public void initialisationTableRoutage(Machine machine) {
-    }
-
-    public String affichageSeparateurTableRoutage() {
-        
-        String separateurTableRoutage = "";
-        for (int i = 0; i < LONG_TOTALE + 4; i++) {
-            if (i == 0 || i == LONG_MAX_ADRESSE_IP + 1 || i == LONG_MAX_ADRESSE_IP + LONG_MAX_PASSERELLE + 2 || i == LONG_TOTALE + 3) {
-                separateurTableRoutage += "+";
-            }
-            else {
-                separateurTableRoutage += "-";
-            }
-        }
-        return separateurTableRoutage;
-    }
-
-    /**
-    * @param contenus tableau de 3 données de type String que l'on veut afficher dans la table ARP 
-    */
-    public String affichageContenuTableRoutage(String[] contenus) {
-        
-        String contenuTableRoutage = "";
-        for (int i = 0; i < LONG_TOTALE + 4; i++) {
-            if (i == 0 || i == LONG_MAX_ADRESSE_IP + 1 || i == LONG_MAX_ADRESSE_IP + LONG_MAX_PASSERELLE + 2 || i == LONG_TOTALE + 3) {
-                contenuTableRoutage += "|";
-            }
-            else if (i == 1) {
-                contenuTableRoutage += contenus[0];
-                i += contenus[0].length() - 1;
-            }
-            else if (i == LONG_MAX_ADRESSE_IP + 2) {
-                contenuTableRoutage += contenus[1];
-                i += contenus[1].length() - 1;
-            }
-            else if (i == LONG_MAX_ADRESSE_IP + LONG_MAX_PASSERELLE + 3) {
-                contenuTableRoutage += contenus[2];
-                i += contenus[2].length() - 1;
-            }
-            else {
-                contenuTableRoutage += " ";
-            }
-        }
-
-        return contenuTableRoutage;
-    }
-
-    // Permet d'afficher la table ARP
-    public void afficher() {
-        
-        System.out.println(this.toString());
-    }
-
-    // Affichage d'une instance de classe TableARP
-    @Override
-    public String toString() {
-        
-        String enTete = "";
-        enTete += this.affichageSeparateurTableRoutage() + "\n";
-        
-        enTete += this.affichageContenuTableRoutage(this.contenuEnTeteAffichage) + "\n";            
-        enTete += this.affichageSeparateurTableRoutage() + "\n";
-
-        String corps = "";
-        int cpt = 0;
-        for (Map.Entry<Machine, String[]> entree : this.tableRoutage.entrySet()) {
-                
-            String[] contenusCorps = {entree.getValue()[0], entree.getValue()[1], entree.getValue()[2]};
-            corps += this.affichageContenuTableRoutage(contenusCorps) + "\n";
-            
-            if (cpt == this.tableRoutage.size() - 1) { 
-                corps += this.affichageSeparateurTableRoutage();
-            }
-            else {
-                corps += this.affichageSeparateurTableRoutage() + "\n";
-            }
-            cpt++;
-        }
-
-        String tableRoutage = "";
-        tableRoutage += enTete + corps;
-
-        return tableRoutage;
-    }
+   
 }
 
