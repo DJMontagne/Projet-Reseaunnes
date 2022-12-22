519ec36722a993a149e9a4f2a7da6131539f7f34
Audureau Victor <victor.audureau@etudiant.univ-lr.fr>
2022-12-22 16:05:29 +0100

    Update Machine.java

--- Projet/src/main/java/General/Machine.java
+++ Projet/src/main/java/General/Machine.java
@@ -13,7 +13,7 @@ public abstract class Machine {
     protected ArrayList<CarteReseau> cartesR; //Liste de cartes réseau
     private TableARP tableARP; //table ARP de la machine
     private TableRoutage tableRoutage; //table de routage de la machine
-    private ArrayList<Terminal> terminaux; //liste des terminaux de la machine(pour les commandes)
+    private Terminal terminal; //terminal de la machine
     private int x;
     private int y;
     private static int nbrMachine;
@@ -25,8 +25,7 @@ public abstract class Machine {
         Machine.nbrMachine++;
         this.id = nbrMachine;
         cartesR = new ArrayList<CarteReseau>(); //création d'une liste vide de carteReseau
-        terminaux = new ArrayList<Terminal>();  // création d'un liste vide de Terminal
-        terminaux.add(new Terminal(this)); //ajout d'un terminal dans la liste
+        terminal = new Terminal(this);  //ajout d'un terminal
         tableARP = new TableARP();         // création d'une tableARP vierge
         tableRoutage = new TableRoutage(); //création d'une tableRoutage vierge(à voir avec le constructeur pour le 0.0.0.0/0)
         this.ports = this.initPorts();
@@ -35,11 +34,9 @@ public abstract class Machine {
     }
     
     
-    public void addTerminal(Terminal term){ //ajout d'un terminal dans la liste de terminaux
-        terminaux.add(term);
-    }
-    public void fermerTerminal(int index){ //retire le terminal de la liste des terminaux de la machine, le rendant inutilisable
-        this.terminaux.get(index).fermerTerminal();
+    
+    public void fermerTerminal(){ //retire le terminal de la liste des terminaux de la machine, le rendant inutilisable
+        this.terminal.fermerTerminal();
     }
     
     //----LISTE DE GETTERS ET SETTERS (peut-être à renommer pour facilité)-----
@@ -90,8 +87,8 @@ public abstract class Machine {
         this.tableRoutage = tableRoutage;
     }
 
-    public ArrayList<Terminal> getTerminaux() {
-        return terminaux;
+    public Terminal getTerminal() {
+        return terminal;
     }
     
     public CarteReseau[] getPorts() {
